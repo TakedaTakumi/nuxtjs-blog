49b85202a1dfc88d3d3037133b85ff79d9e68337
Takeda Takumi <56464951+TakedaTakumi@users.noreply.github.com>
2024-07-18 15:07:20 +0000

    feat: add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint'
+  ],
+  typescript: { 
+    typeCheck: true
+  },
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: true
+    }
+  },
 })
