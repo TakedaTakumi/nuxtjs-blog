7e16a3d9ed1ceda1048ce40735be8b1ed10e253f
Takeda Takumi <56464951+TakedaTakumi@users.noreply.github.com>
2024-07-31 14:47:05 +0000

    feat: add theme switch

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <script setup lang='ts'>
-import { useTheme } from 'vuetify';
 
-const theme = useTheme();
-
-theme.global.name.value = 'light';
-// theme.global.name.value = 'dark';
 </script>
 
 <template>
--- components/scenario/Header.vue
+++ components/scenario/Header.vue
@@ -1,5 +1,15 @@
 <script setup lang='ts'>
+import { useTheme } from 'vuetify';
 
+const theme = useTheme();
+
+const value = ref(true);
+
+watch(value, () => {
+  theme.global.name.value = value.value ? 'light' : 'dark';
+});
+
+value.value = false;
 </script>
 
 <template>
@@ -7,6 +17,14 @@
     <v-app-bar-title>
       <lazy-app-title />
     </v-app-bar-title>
+
+    <template #append>
+      <v-switch
+        v-model="value"
+        label="☀"
+        color="primary"
+      />
+    </template>
   </v-app-bar>
 </template>
 
